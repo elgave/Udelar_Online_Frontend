eb29f87f5b1b13e8ab143fee0e512b05877d79cc
fuzzfactory0 <zekrom325@gmail.com>
2020-11-15 22:48:16 -0300

    Modulo docentes

--- src/app/components/facultad/curso/curso.component.ts
+++ src/app/components/facultad/curso/curso.component.ts
@@ -7,6 +7,11 @@ import * as env from 'src/env';
 import { MatDialog } from "@angular/material/dialog";
 import { AddSeccionComponent } from './seccion/add-seccion/add-seccion.component';
 import { AddComponenteComponent } from './componente/add-componente/add-componente.component';
+import { DeleteSeccionComponent } from './seccion/delete-seccion/delete-seccion.component';
+import { DeleteComponenteComponent } from './componente/delete-componente/delete-componente.component';
+import { EditSeccionComponent } from './seccion/edit-seccion/edit-seccion.component';
+import { EditComponenteComponent } from './componente/edit-componente/edit-componente.component';
+import { RenombrarCursoComponent } from './editar/renombrar-curso/renombrar-curso.component';
 
 @Component({
   selector: 'app-curso',
@@ -19,6 +24,8 @@ export class CursoComponent implements OnInit {
   rol: string;
   isCurso: boolean;
   isDocente: boolean;
+  isAdmin: boolean;
+  isEstudiante: boolean;
   curso: Curso;
 
   @Input() cursoId: number;
@@ -33,6 +40,7 @@ export class CursoComponent implements OnInit {
         s.componentes.sort((a, b) => a.indice > b.indice ? 1 : -1);
       })
     });
+    this.isAdmin = sessionStorage.getItem('rol') == 'administrador'
   }
 
   loadCurso() {
@@ -42,7 +50,7 @@ export class CursoComponent implements OnInit {
       this.curso.secciones.forEach(s => {
         s.componentes.sort((a, b) => a.indice > b.indice ? 1 : -1);
       })
-      this.isDocente = this.curso.docentes.find(u => u.cedula == sessionStorage.getItem('cedula')) != null;
+      this.isDocente = this.curso.docentes.find(u => u.cedula == sessionStorage.getItem('cedula')) != null && sessionStorage.getItem('rol') == 'docente';
     }); 
   }
 
@@ -50,14 +58,65 @@ export class CursoComponent implements OnInit {
     this.loadCurso();
   }
 
-  editCurso() {
+  editCurso(cursoId: number, cursoNombre: string) {
+    let dialogRef = this.dialog.open(RenombrarCursoComponent, {
+      width: '540px',
+      height: '350px',
+      data: { cursoId: cursoId, cursoNombre: cursoNombre, confirmaBedelia: this.curso.confirmaBedelia, creditos: this.curso.cantCreditos, isAdmin: this.isAdmin }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.loadCurso();
+    });
+  }
 
+  editSeccion(seccionId: string, seccionNombre: string, indice: number) {
+    let dialogRef = this.dialog.open(EditSeccionComponent, {
+      width: '540px',
+      height: '350px',
+      data: { seccionId: seccionId, seccionNombre: seccionNombre, seccionIndice: indice }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.loadCurso();
+    });
+  }
+
+  editComponente(componenteId: string, componenteNombre: string, indice: number) {
+    let dialogRef = this.dialog.open(EditComponenteComponent, {
+      width: '540px',
+      height: '350px',
+      data: { componenteId: componenteId, componenteNombre: componenteNombre, componenteIndice: indice }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.loadCurso();
+    });
+  }
+
+  deleteSeccion(seccionId:number, seccionNombre:string) {
+    let dialogRef = this.dialog.open(DeleteSeccionComponent, {
+      width: '540px',
+      height: '300px',
+      data: { seccionId: seccionId, seccionNombre: seccionNombre, cursoNombre: this.curso.nombre }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.loadCurso();
+    });
+  }
+ 
+  deleteComponente(componenteId:number, seccionNombre:string, componenteNombre:string) {
+    let dialogRef = this.dialog.open(DeleteComponenteComponent, {
+      width: '540px',
+      height: '300px',
+      data: { componenteId: componenteId, seccionNombre: seccionNombre, componenteNombre: componenteNombre }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.loadCurso();
+    });
   }
 
   addSeccion() {
     let dialogRef = this.dialog.open(AddSeccionComponent, {
       width: '540px',
-      height: '380px',
+      height: '310px',
       data: { cursoId: this.curso.id }
     });
     dialogRef.afterClosed().subscribe(result => {
